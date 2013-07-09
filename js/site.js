055d03ad39e93ce67d7d10d27b473866cac6ded7
Jeremy Holsted <jeremy@taylorpond.com>
2013-07-09 11:12:40 -0700

    Adding javascript to change "no-js" class to "js"

--- js/site.js
+++ js/site.js
@@ -1,4 +1,7 @@
 
+// Change 'no-js' class to 'js' on the html element
+document.documentElement.className = document.documentElement.className.replace(/\bno-js\b/g, '') + 'js';
+
 	jQuery(document).ready(function($) {
 
 		// Your JavaScript goes here
